8df7955a9edc75c340b219f7fee2c5bfdc6fa237
Sharayu Mane <sharumane@gmail.com>
2022-08-20 14:47:00 +0200

    test author change

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Sherdu')
+    print_hi('Test author')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
